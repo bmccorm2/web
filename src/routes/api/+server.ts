3f4044600e8715b926d79a9a3372ae066697cfeb
Bryan McCormick <bryan@1mccormick.com>
2024-01-15 18:36:38 -0700

    a

--- src/routes/api/+server.ts
+++
@@ -1,6 +0,0 @@
-import type { Config } from "@sveltejs/adapter-vercel";
-export const config: Config = { runtime: "edge" };
-
-export const GET = async () => {
-  return new Response(`Hello world! - ${process.env.VERCEL_REGION}`);
-};
