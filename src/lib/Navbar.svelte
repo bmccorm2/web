48155e96c4434f8cc1278c9c4a60b6e10e51f18d
Bryan McCormick <bryan@1mccormick.com>
2024-11-08 14:06:13 -0700

    change dark mode icon

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -3,9 +3,8 @@
 	import { Terminal, Menu } from 'lucide-svelte';
 	import Sun from 'lucide-svelte/icons/sun';
 	import Moon from 'lucide-svelte/icons/moon';
-	import { resetMode, setMode } from 'mode-watcher';
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { Button } from './components/ui/button';
+	import { toggleMode } from 'mode-watcher';
 
 	let url = $derived($page.url.pathname);
 	let activeUrl = $derived(url === '/' ? '/home' : url);
@@ -79,24 +78,20 @@
 			</div>
 		{/if}
 		<li class="pl-6">
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger
-					class={`${buttonVariants({ variant: 'outline', size: 'icon' })} border-0 bg-gray-300 dark:bg-slate-800`}
-				>
-					<Sun
-						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-					/>
-					<Moon
-						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-					/>
-					<span class="sr-only">Toggle theme</span>
-				</DropdownMenu.Trigger>
-				<DropdownMenu.Content align="end">
-					<DropdownMenu.Item onclick={() => setMode('light')}>Light</DropdownMenu.Item>
-					<DropdownMenu.Item onclick={() => setMode('dark')}>Dark</DropdownMenu.Item>
-					<DropdownMenu.Item onclick={() => resetMode()}>System</DropdownMenu.Item>
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
+			<Button
+				onclick={toggleMode}
+				variant="outline"
+				size="icon"
+				class="border-0 bg-gray-300 dark:bg-slate-800 dark:hover:bg-slate-900"
+			>
+				<Sun
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+				/>
+				<Moon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</Button>
 		</li>
 		<li class="md:hidden">
 			<button onclick={toggleNavbar}>
